0d18d7f9f7c7d1c609baefe007a3485e51a226fc
Trent Weiss <ttw2xk@virginia.edu>
2017-11-21 15:25:32 -0500

    add gradient calculation to test program

--- src/test_program.cpp
+++ src/test_program.cpp
@@ -1,13 +1,14 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 #include <string>
 using namespace cv;
 using namespace std;
 int main( int argc, char** argv )
 {
-    String imageName( "../data/HappyFish.jpg" ); // by default
+    String imageName( "../data/eagle.jpg" ); // by default
     if( argc > 1)
     {
         imageName = argv[1];
@@ -21,6 +22,34 @@ int main( int argc, char** argv )
     }
     namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
     imshow( "Display window", image );                // Show our image inside it.
+
+  Mat src_gray;
+  Mat grad;
+  char* window_name = "Sobel Demo - Simple Edge Detector";
+  int scale = 1;
+  int delta = 0;
+  int ddepth = CV_16S;
+  cvtColor( image, src_gray, CV_BGR2GRAY ); 
+  namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+  /// Generate grad_x and grad_y
+  Mat grad_x, grad_y;
+  Mat abs_grad_x, abs_grad_y;
+
+  /// Gradient X
+  //Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
+  Sobel( src_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
+  convertScaleAbs( grad_x, abs_grad_x );
+
+  /// Gradient Y
+  //Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
+  Sobel( src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
+  convertScaleAbs( grad_y, abs_grad_y );
+
+  /// Total Gradient (approximate)
+  addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
+
+  imshow( window_name, grad );
+
     waitKey(0); // Wait for a keystroke in the window
     return 0;
 }
