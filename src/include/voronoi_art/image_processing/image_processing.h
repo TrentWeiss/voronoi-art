3e3bc1ed760ec50a609c3d16cf9649d1b19fb911
Trent Weiss <ttw2xk@virginia.edu>
2017-12-19 12:23:47 -0500

    Update image_processing.h

--- src/include/voronoi_art/image_processing/image_processing.h
+++ src/include/voronoi_art/image_processing/image_processing.h
@@ -9,7 +9,7 @@ This software is provided by the copyright holders and contributors “as is” 
 In no event shall copyright holders or contributors be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; 
 or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
  *  Created on: Nov 22, 2017
- *      Author: trent
+ *      Author: Trent Weiss
  */
 
 #ifndef INCLUDE_IMAGE_PROCESSING_H_
